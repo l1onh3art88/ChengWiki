f90c36aab24b26e3ef555247b95011c16f01473b
l1on3hart88 <spenccheng@gmail.com>
2015-11-02 14:00:18 +0000

    completed login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  default_url_options :host => "www.chengwiki.herokuapp.com"
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
